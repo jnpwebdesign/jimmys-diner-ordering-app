e0d18b957b8f0e76f305f8c9d5ba157fd966e2c9
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-11-19 12:11:32 -0500

    11/19/22 12:11 draft

--- restaurant-js.js
+++ restaurant-js.js
@@ -1,3 +1,27 @@
 import { menuArray } from "./data.js";
 
 console.log(menuArray);
+
+
+const modalForm = document.getElementById("modal-form");
+
+
+
+modalForm.style.display="flex";
+
+modalForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let customerPaymentInfo = new FormData(modalForm);
+    const customerName = customerPaymentInfo.get("name-input");
+    const creditCardNumber = customerPaymentInfo.get("card-number-input");
+    const cvv = customerPaymentInfo.get("cvv-input");
+    console.log(customerName, creditCardNumber, cvv, customerPaymentInfo);
+    modalForm.style.display = "none";
+    document.getElementById("thank-you-container").style.display="flex";
+    document.getElementById("thank-you-message").style.display="flex";
+    return customerPaymentInfo;
+
+})
+
+
